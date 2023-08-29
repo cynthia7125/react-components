989973a8891896f6a69e9c8a2f7f3729583e3911
Reyhana Cherop <reyhanacynthia@gmail.com>
2023-08-29 16:52:43 +0300

    Setup withAuth HOC

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,19 @@
 import Header from "./Header";
 import Speakers from "./Speakers";
 import Layout from "./Layout";
+import { AuthProvider } from "../contexts/AuthContext";
 
 function App() {
   return (
-    <Layout startingTheme={"light"}>
+    <AuthProvider initialLoggesInUser="Ronald" >
+      <Layout startingTheme={"light"}>
       <div>
         <Header />
         <Speakers />
       </div>
     </Layout>
+    </AuthProvider>
+    
   );
 }
 
